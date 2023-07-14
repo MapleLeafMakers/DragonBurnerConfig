fe8c580d961a178d1c7b4efadd8debf961d0f910
chirpy2605 <117446115+chirpy2605@users.noreply.github.com>
2023-07-14 16:57:12 +0100

    Update quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -95,7 +95,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
       config: {
-        dark: true,
+        dark: false,
       },
 
       // iconSet: 'material-icons', // Quasar icon set
